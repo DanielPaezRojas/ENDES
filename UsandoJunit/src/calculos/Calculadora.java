344278ba0878f5c1862e3578a61a7a9eab8eb2e3
DanielPaezRojas <95438007+DanielPaezRojas@users.noreply.github.com>
2022-04-28 19:19:09 +0200

    Add files via upload

---
+++ UsandoJunit/src/calculos/Calculadora.java
@@ -0,0 +1,10 @@
+package calculos;
+
+public class Calculadora {
+	public static int restar(int num1, int num2) {
+		return num2-num1;
+	}
+	public static int dividir(int num1, int num2) {
+		return num1/num2;
+	}
+}
---
+++ UsandoJunit/src/calculos/CalculadoraTest.java
@@ -0,0 +1,14 @@
+package calculos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@Test
+	void testRestart() {
+		fail("Not yet implemented");
+	}
+
+}
